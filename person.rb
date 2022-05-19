c94d408867e374417ae5c877c16dd8f5e6f957c1
Banstein <islambahnas1@gmail.com>
2022-05-19 22:30:06 +0200

    Add cap, strip, decorator methods

--- person.rb
+++ person.rb
@@ -1,8 +1,14 @@
-class Person
+require './nameable'
+require './capitalize_decorator'
+require './trimmer_decorator'
+
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -10,10 +16,14 @@ class Person
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 
-  private
+  def correct_name
+    @name
+  end
+
+  # private
 
   def of_age?
     @age >= 18
