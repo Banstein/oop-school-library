358bb12c00f2454bcc0a98aac15f2b73ceafb758
Banstein <islambahnas1@gmail.com>
2022-05-21 20:43:34 +0200

    add basic UI

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require './student'
 
 class Teacher < Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
